f6aeaea037b24ca1dea30c67bed22db21732663b
jeroscafati <jeroscafati@gmail.com>
2025-10-13 18:32:06 -0300

    router signature changed from filename to file_path

--- backend/app/routers/plot.py
+++ backend/app/routers/plot.py
@@ -15,49 +15,47 @@ class BandsPerOctave(int, Enum):
     twenty_four = 24
     forty_eight = 48
 
-@router.get("/plot/{filename}")
-async def get_plot_data(filename: str):
-    file_key = f"uploads/{filename}"
-    
-    file_stream = download_file_from_s3(file_key)
+@router.get("/plot/{file_path:path}")
+async def get_plot_data(file_path: str):
+
+    file_stream = download_file_from_s3(file_path)
     y, sr = librosa.load(file_stream, sr=None, mono=True)
     plot_data = plot_waveform(y, sr)
     
     return plot_data
 
-@router.get("/spectrogram/{filename}")
-async def get_spectrogram_data(filename: str):
-    file_key = f"uploads/{filename}"
-    file_stream = download_file_from_s3(file_key)
+@router.get("/spectrogram/{file_path:path}")
+async def get_spectrogram_data(file_path: str):
+
+    file_stream = download_file_from_s3(file_path)
 
     y, sr = librosa.load(file_stream, sr=None, mono=True)
     plot_data = plot_spectrogram(y, sr)
     
     return plot_data
 
-@router.get("/csd/{filename}")
+@router.get("/csd/{file_path:path}")
 async def get_csd_data(
-    filename: str,
+    file_path: str,
     bands: BandsPerOctave = BandsPerOctave.twenty_four):
-    file_key = f"uploads/{filename}"
-    file_stream = download_file_from_s3(file_key)
+    
+    file_stream = download_file_from_s3(file_path)
 
     y, sr = librosa.load(file_stream, sr=None, mono=True)
     plot_data = plot_csd(y, sr, bands_per_oct=bands.value)
 
     return plot_data
 
-@router.get("/frequency-response/{filename}")
+@router.get("/frequency-response/{file_path:path}")
 async def get_frequency_response_data(
-    filename: str,
+    file_path: str,
     bands: BandsPerOctave = BandsPerOctave.twenty_four):
     """
     Provides data for frequency response plot.
     - **filename**: The name of the uploaded audio file.
     - **bands**: The number of bands per octave for smoothing.
     """
-    file_key = f"uploads/{filename}"
-    file_stream = download_file_from_s3(file_key)
+    file_stream = download_file_from_s3(file_path)
 
     y, sr = librosa.load(file_stream, sr=None, mono=True)
 
